a1888b2acfdcdfe0fe0140ece5884bf505dc80c2
Allan Smeyatsky <asmeyatsky@hotmail.com>
2026-03-30 16:46:58 +0200

    fix: use pip3/python3 for compatibility

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -e .
-python -m uvicorn synaptic_bridge.presentation.api.main:app --reload
+pip3 install -e .
+python3 -m uvicorn synaptic_bridge.presentation.api.main:app --reload
